42b1e976819e710a8432596ef6928e601f18df47
Yves de Sá <yvesdsb98@gmail.com>
2022-05-14 23:58:57 -0300

    feat: start project spring boot

---
+++ src/main/java/com/dio/bankchallenge/BankChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.dio.bankchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankChallengeApplication.class, args);
+	}
+
+}
