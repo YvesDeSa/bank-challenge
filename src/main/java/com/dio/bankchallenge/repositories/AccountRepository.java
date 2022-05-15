18434de5ac31926fc62cc0d42e92952fcfd59f38
Yves de Sá <yvesdsb98@gmail.com>
2022-05-15 02:30:30 -0300

    feat: create application controllers

--- src/main/java/com/dio/bankchallenge/repositories/AccountRepository.java
+++ src/main/java/com/dio/bankchallenge/repositories/AccountRepository.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Repository;
 import ch.qos.logback.core.net.server.Client;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.dio.bankchallenge.model.Account;
 
@@ -18,7 +16,6 @@ public interface AccountRepository extends JpaRepository<Account, UUID> {
 
   public Optional<Account> findById(UUID id);
 
-  @Query("SELECT * FROM tb_client WHERE tb_client.id = :id")
-  public Client findByClient(@Param("id") UUID id);
+  public Client findByClient(UUID id);
 
 }
--- src/main/java/com/dio/bankchallenge/service/AccountService.java
+++ src/main/java/com/dio/bankchallenge/service/AccountService.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 @Service
 public class AccountService {
+
   @Autowired
   private AccountRepository accountRepository;
 
